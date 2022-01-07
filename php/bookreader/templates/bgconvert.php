87717fbcb8c3a23ed20b3aec947bc45e1ffc888c
Shivashankar <shivably04sdst@gmail.com>
2022-01-07 13:35:42 +0530

    option added to generate jpg/1/ images from pdf in bgconvert

--- php/bookreader/templates/bgconvert.php
+++ php/bookreader/templates/bgconvert.php
@@ -10,27 +10,52 @@
 
 	if($reduce == 1)
 	{
+		$pdfurl = "../../../Volumes/PDF/".$bookID;
 		$imgurl = "../../../Volumes/jpg/1/".$bookID;
+		$dzimgurl = "../../../Volumes/jpg/2/".$bookID;
 		$scale = 2100;
 		$djvurl = "../../../Volumes/djvu/".$bookID;
 		$tifurl = "../../../Volumes/tif/".$bookID;
 		
-		if(!file_exists($tifurl."/".$img[0].".tif"))
-		{
-			$cmd = "ddjvu -format=tif ".$djvurl."/".$img[0].".djvu ".$tifurl."/".$img[0].".tif";
-			exec($cmd);
+
+		if(file_exists($pdfurl."/index.pdf")){
+
+			$images = scandir($dzimgurl);
+			$jpgimages = [];
+
+			for($i=0;$i<count($images);$i++)
+			{
+				if($images[$i] != '.' && $images[$i] != '..' && preg_match('/(\.jpg)/' , $images[$i]))
+				{
+					array_push($jpgimages,$images[$i]);
+				}
+			}	
+			
+			$page = $img[0] . ".jpg";
+			$arrayIndex = array_search($page,$jpgimages);
+			
+			$cmd = "convert -density 300 -resize x" . $scale . " " . $pdfurl . "/index.pdf[" . $arrayIndex . "] -alpha remove " . $imgurl . "/". $page;
+			exec($cmd);	
 		}
-		if(!file_exists($imgurl."/".$img[0].".jpg"))
-		{
-			$cmd="convert $tifurl/".$img[0].".tif -resize x".$scale." $imgurl/".$img[0].".jpg";
-			exec($cmd);
+		else{
+		
+			if(!file_exists($tifurl."/".$img[0].".tif"))
+			{
+				$cmd = "ddjvu -format=tif ".$djvurl."/".$img[0].".djvu ".$tifurl."/".$img[0].".tif";
+				exec($cmd);
+			}
+			if(!file_exists($imgurl."/".$img[0].".jpg"))
+			{
+				$cmd="convert $tifurl/".$img[0].".tif -resize x".$scale." $imgurl/".$img[0].".jpg";
+				exec($cmd);
+			}
 		}
 	}
 	$array['id'] = "#pagediv".$index;
 	$array['mode'] = $mode;
 	$array['img'] = $imgurl."/".$img[0].".jpg";
 	
-	echo json_encode($array, JSON_UNESCAPED_SLASHES);
+	echo json_encode($array);
 	//~ Update manifest file to download the request file.
 	$myfile = fopen("appcache.manifest", "w") or die("Unable to open file!!!");
 	fwrite($myfile,"CACHE MANIFEST\n");
