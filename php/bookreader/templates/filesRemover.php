c400c91b7b70d215bae810c16e0b672ae9656f53
Sriraghunandana92 <raghunandanakeremane@gmail.com>
2024-05-23 10:16:47 +0530

    changes in the path

--- php/bookreader/templates/filesRemover.php
+++ php/bookreader/templates/filesRemover.php
@@ -1,8 +1,8 @@
 <?php
-	$jpg = 'find ../../../Volumes/jpg/2/ -mmin +10 -type f -name "*.jpg" -exec rm {} \;';
+	$jpg = 'find ../../../Volumes/jpg/1/ -mmin +10 -type f -name "*.jpg" -exec rm {} \;';
 	exec($jpg);
 	$tif = 'find ../../../Volumes/tif/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
 	exec($tif);
-	$pdf = 'find ../../../Volumes/PDF/ -mmin +5 -type f -name "*.pdf" -exec rm {} \;';
+	$pdf = 'find ../../../ReadWrite/ -mmin +5 -type f -name "*.pdf" -exec rm {} \;';
 	exec($pdf);
 ?>
